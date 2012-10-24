c9e0450943cb7e08601cbc58f214b7b183181a58
Raun <raun@C700.(none)>
2012-10-24 12:13:05 +0530

    Added playGame

--- ps4a.py
+++ ps4a.py
@@ -256,15 +256,27 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print "playGame not yet implemented." # <-- Remove this line when you code the function
-   
+    preChoice = 'f'
+    while True:
+    	choice = str(raw_input('Enter n to deal a new hand, r to replay the last hand, or e to end game: '))
+    	if choice == 'n':
+    		hand=dealHand(HAND_SIZE)
+    		handCopy=hand.copy()
+    		playHand(handCopy,wordList,HAND_SIZE)
+    		print
+    		preChoice = choice
+    	elif choice == 'r': 
+		if preChoice == 'f':
+    			print 'You have not played a hand yet. Please play a new hand first!'
+    			print
+    		else:
+    			handCopy=hand.copy()
+   			playHand(handCopy,wordList,HAND_SIZE)
+   			print
+    	elif choice == 'e':
+    		return
+    	else:
+    		print 'Invalid command.'
 
-
-
-#
-# Build data structures used for entire session and play game
-#
-if __name__ == '__main__':
-    wordList = loadWords()
-    playGame(wordList)
+wordList = loadWords()
+playGame(wordList)
