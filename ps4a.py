8f009f628c6e204498135235fcc43f199d4626ef
Anshul Sharma <anshul.jmi@gmail.com>
2012-10-24 12:16:58 +0530

    Update ps4a.py

--- ps4a.py
+++ ps4a.py
@@ -1,16 +1,10 @@
-# 6.00x Problem Set 4A Template
-#
-# The 6.00 Word Game
-# Created by: Kevin Luu <luuk> and Jenna Wiens <jwiens>
-# Modified by: Sarina Canelake <sarina>
-#
 
 import random
 import string
 
 VOWELS = 'aeiou'
-CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
-HAND_SIZE = 7
+CONSONANTS = ''bcdfghjklmnpqrstvwxyz
+HAND_SIZE =7 
 
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
